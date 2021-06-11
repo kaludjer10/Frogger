848b4cb77dde2a0042a82f901de56f3bc89db5f1
kaludjer10 <85765556+kaludjer10@users.noreply.github.com>
2021-06-11 23:39:47 +0200

    Add files via upload

---
+++ ФРОГГЕР/ФРОГГЕР/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace ФРОГГЕР
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.car7 = new System.Windows.Forms.PictureBox();
+            this.car8 = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car4 = new System.Windows.Forms.PictureBox();
+            this.car3 = new System.Windows.Forms.PictureBox();
+            this.car5 = new System.Windows.Forms.PictureBox();
+            this.car6 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.kamion1 = new System.Windows.Forms.PictureBox();
+            this.kamion3 = new System.Windows.Forms.PictureBox();
+            this.kamion2 = new System.Windows.Forms.PictureBox();
+            this.voda1 = new System.Windows.Forms.PictureBox();
+            this.zabac = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.jkra = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.voda1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zabac)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jkra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(88, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 50);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(88, 178);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(225, 50);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(88, 225);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(225, 50);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(447, 131);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(225, 50);
+            this.panel4.TabIndex = 3;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(447, 178);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(225, 50);
+            this.panel5.TabIndex = 4;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(447, 225);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(225, 50);
+            this.panel6.TabIndex = 5;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(803, 131);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(225, 50);
+            this.panel7.TabIndex = 6;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(803, 178);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(225, 50);
+            this.panel8.TabIndex = 7;
+            this.panel8.Paint += new System.Windows.Forms.PaintEventHandler(this.panel8_Paint);
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(803, 225);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(225, 50);
+            this.panel9.TabIndex = 8;
+            this.panel9.Paint += new System.Windows.Forms.PaintEventHandler(this.panel9_Paint);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::ФРОГГЕР.Properties.Resources.улица3;
+            this.pictureBox11.Location = new System.Drawing.Point(-9, 334);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(1108, 324);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 19;
+            this.pictureBox11.TabStop = false;
+            // 
+            // car7
+            // 
+            this.car7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car7.Location = new System.Drawing.Point(915, 341);
+            this.car7.Name = "car7";
+            this.car7.Size = new System.Drawing.Size(113, 50);
+            this.car7.TabIndex = 18;
+            this.car7.TabStop = false;
+            // 
+            // car8
+            // 
+            this.car8.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car8.Location = new System.Drawing.Point(915, 581);
+            this.car8.Name = "car8";
+            this.car8.Size = new System.Drawing.Size(113, 50);
+            this.car8.TabIndex = 17;
+            this.car8.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car2.Location = new System.Drawing.Point(88, 581);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(113, 50);
+            this.car2.TabIndex = 16;
+            this.car2.TabStop = false;
+            // 
+            // car4
+            // 
+            this.car4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car4.Location = new System.Drawing.Point(379, 581);
+            this.car4.Name = "car4";
+            this.car4.Size = new System.Drawing.Size(113, 50);
+            this.car4.TabIndex = 15;
+            this.car4.TabStop = false;
+            // 
+            // car3
+            // 
+            this.car3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car3.Location = new System.Drawing.Point(379, 341);
+            this.car3.Name = "car3";
+            this.car3.Size = new System.Drawing.Size(113, 50);
+            this.car3.TabIndex = 14;
+            this.car3.TabStop = false;
+            // 
+            // car5
+            // 
+            this.car5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car5.Location = new System.Drawing.Point(659, 341);
+            this.car5.Name = "car5";
+            this.car5.Size = new System.Drawing.Size(113, 50);
+            this.car5.TabIndex = 13;
+            this.car5.TabStop = false;
+            // 
+            // car6
+            // 
+            this.car6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car6.Location = new System.Drawing.Point(659, 581);
+            this.car6.Name = "car6";
+            this.car6.Size = new System.Drawing.Size(113, 50);
+            this.car6.TabIndex = 12;
+            this.car6.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.car1.Location = new System.Drawing.Point(88, 341);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(113, 50);
+            this.car1.TabIndex = 11;
+            this.car1.TabStop = false;
+            // 
+            // kamion1
+            // 
+            this.kamion1.BackColor = System.Drawing.Color.Red;
+            this.kamion1.Location = new System.Drawing.Point(88, 473);
+            this.kamion1.Name = "kamion1";
+            this.kamion1.Size = new System.Drawing.Size(225, 50);
+            this.kamion1.TabIndex = 10;
+            this.kamion1.TabStop = false;
+            // 
+            // kamion3
+            // 
+            this.kamion3.BackColor = System.Drawing.Color.Red;
+            this.kamion3.Location = new System.Drawing.Point(803, 473);
+            this.kamion3.Name = "kamion3";
+            this.kamion3.Size = new System.Drawing.Size(225, 50);
+            this.kamion3.TabIndex = 9;
+            this.kamion3.TabStop = false;
+            // 
+            // kamion2
+            // 
+            this.kamion2.BackColor = System.Drawing.Color.Red;
+            this.kamion2.Location = new System.Drawing.Point(447, 473);
+            this.kamion2.Name = "kamion2";
+            this.kamion2.Size = new System.Drawing.Size(225, 50);
+            this.kamion2.TabIndex = 20;
+            this.kamion2.TabStop = false;
+            this.kamion2.Click += new System.EventHandler(this.kamion2_Click);
+            // 
+            // voda1
+            // 
+            this.voda1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.voda1.Location = new System.Drawing.Point(0, 131);
+            this.voda1.Name = "voda1";
+            this.voda1.Size = new System.Drawing.Size(1090, 144);
+            this.voda1.TabIndex = 22;
+            this.voda1.TabStop = false;
+            // 
+            // zabac
+            // 
+            this.zabac.BackColor = System.Drawing.Color.Lime;
+            this.zabac.Location = new System.Drawing.Point(533, 664);
+            this.zabac.Name = "zabac";
+            this.zabac.Size = new System.Drawing.Size(25, 25);
+            this.zabac.TabIndex = 23;
+            this.zabac.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1099, 609);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // jkra
+            // 
+            this.jkra.BackColor = System.Drawing.Color.Green;
+            this.jkra.Location = new System.Drawing.Point(0, -1);
+            this.jkra.Name = "jkra";
+            this.jkra.Size = new System.Drawing.Size(1090, 126);
+            this.jkra.TabIndex = 25;
+            this.jkra.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 720);
+            this.Controls.Add(this.zabac);
+            this.Controls.Add(this.kamion2);
+            this.Controls.Add(this.car7);
+            this.Controls.Add(this.car8);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.car4);
+            this.Controls.Add(this.car3);
+            this.Controls.Add(this.car5);
+            this.Controls.Add(this.car6);
+            this.Controls.Add(this.car1);
+            this.Controls.Add(this.kamion1);
+            this.Controls.Add(this.kamion3);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.voda1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.jkra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamion2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.voda1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zabac)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jkra)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.PictureBox kamion3;
+        private System.Windows.Forms.PictureBox kamion1;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.PictureBox car6;
+        private System.Windows.Forms.PictureBox car5;
+        private System.Windows.Forms.PictureBox car3;
+        private System.Windows.Forms.PictureBox car4;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car8;
+        private System.Windows.Forms.PictureBox car7;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox kamion2;
+        private System.Windows.Forms.PictureBox voda1;
+        private System.Windows.Forms.PictureBox zabac;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox jkra;
+    }
+}
+
---
+++ ФРОГГЕР/ФРОГГЕР/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ФРОГГЕР
+{
+    public partial class Form1 : Form
+    {
+        private balvan balv1;
+        private balvan balv2;
+        private balvan balv3;
+        private balvan balv4;
+        private balvan balv5;
+        private balvan balv6;
+        private balvan balv7;
+        private balvan balv8;
+        private balvan balv9;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            balv1 = new balvan(0, 0, panel1.Width, panel1.Height, Color.Brown, Color.Black);
+            balv2 = new balvan(0, 0, panel2.Width, panel2.Height, Color.Brown, Color.Black);
+            balv3 = new balvan(0, 0, panel3.Width, panel3.Height, Color.Brown, Color.Black);
+            balv4 = new balvan(0, 0, panel4.Width, panel4.Height, Color.Brown, Color.Black);
+            balv5 = new balvan(0, 0, panel5.Width, panel5.Height, Color.Brown, Color.Black);
+            balv6 = new balvan(0, 0, panel6.Width, panel6.Height, Color.Brown, Color.Black);
+            balv7 = new balvan(0, 0, panel7.Width, panel7.Height, Color.Brown, Color.Black);
+            balv8 = new balvan(0, 0, panel8.Width, panel8.Height, Color.Brown, Color.Black);
+            balv9 = new balvan(0, 0, panel9.Width, panel9.Height, Color.Brown, Color.Black);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            balv1.NacrtajSe(e.Graphics);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            balv2.NacrtajSe(e.Graphics);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            balv3.NacrtajSe(e.Graphics);
+        }
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            balv4.NacrtajSe(e.Graphics);
+        }
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            balv5.NacrtajSe(e.Graphics);
+        }
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+            balv6.NacrtajSe(e.Graphics);
+        }
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+            balv7.NacrtajSe(e.Graphics);
+        }
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        {
+            balv8.NacrtajSe(e.Graphics);
+        }
+        private void panel9_Paint(object sender, PaintEventArgs e)
+        {
+            balv9.NacrtajSe(e.Graphics);
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //балвани
+            Sudaranje();
+
+            panel1.Left = panel1.Left - 2;
+            if(panel1.Left <= -panel1.Width)
+            {
+                panel1.Left = ClientRectangle.Width;
+            }
+            panel2.Left = panel2.Left + 5;
+            if (panel2.Left >= ClientRectangle.Width)
+            {
+                panel2.Left = -panel2.Width;
+            }
+            panel3.Left = panel3.Left - 2;
+            if (panel3.Left <= -panel3.Width)
+            {
+                panel3.Left = ClientRectangle.Width;
+            }
+
+            panel4.Left = panel4.Left - 2;
+            if (panel4.Left <= -panel4.Width)
+            {
+                panel4.Left = ClientRectangle.Width;
+            }
+            panel5.Left = panel5.Left + 5;
+            if (panel5.Left >= ClientRectangle.Width)
+            {
+                panel5.Left = -panel5.Width;
+            }
+            panel6.Left = panel6.Left - 2;
+            if (panel6.Left <= -panel6.Width)
+            {
+                panel6.Left = ClientRectangle.Width;
+            }
+
+            panel7.Left = panel7.Left - 2;
+            if (panel7.Left <= -panel7.Width)
+            {
+                panel7.Left = ClientRectangle.Width;
+            }
+            panel8.Left = panel8.Left + 5;
+            if (panel8.Left >= ClientRectangle.Width)
+            {
+                panel8.Left = -panel8.Width;
+            }
+            panel9.Left = panel9.Left - 2;
+            if (panel9.Left <= -panel9.Width)
+            {
+                panel9.Left = ClientRectangle.Width;
+            }
+
+            //кола
+
+            car1.Left = car1.Left - 2;
+            if (car1.Left <= -car1.Width)
+            {
+                car1.Left = ClientRectangle.Width;
+            }
+
+            car2.Left = car2.Left - 2;
+            if (car2.Left <= -car2.Width)
+            {
+                car2.Left = ClientRectangle.Width;
+            }
+
+            car3.Left = car3.Left - 2;
+            if (car3.Left <= -car3.Width)
+            {
+                car3.Left = ClientRectangle.Width;
+            }
+
+            car4.Left = car4.Left - 2;
+            if (car4.Left <= -car4.Width)
+            {
+                car4.Left = ClientRectangle.Width;
+            }
+
+            car5.Left = car5.Left - 2;
+            if (car5.Left <= -car5.Width)
+            {
+                car5.Left = ClientRectangle.Width;
+            }
+
+            car6.Left = car6.Left - 2;
+            if (car6.Left <= -car6.Width)
+            {
+                car6.Left = ClientRectangle.Width;
+            }
+
+            car7.Left = car7.Left - 2;
+            if (car7.Left <= -car7.Width)
+            {
+                car7.Left = ClientRectangle.Width;
+            }
+
+            car8.Left = car8.Left - 2;
+            if (car8.Left <= -car8.Width)
+            {
+                car8.Left = ClientRectangle.Width;
+            }
+
+            //камиони
+            kamion1.Left = kamion1.Left + 5;
+            if (kamion1.Left >= ClientRectangle.Width)
+            {
+                kamion1.Left = -kamion1.Width;
+            }
+
+            kamion2.Left = kamion2.Left + 5;
+            if (kamion2.Left >= ClientRectangle.Width)
+            {
+                kamion2.Left = -kamion2.Width;
+            }
+
+            kamion3.Left = kamion3.Left + 5;
+            if (kamion3.Left >= ClientRectangle.Width)
+            {
+                kamion3.Left = -kamion3.Width;
+            }
+
+
+        }
+
+        private void panel4_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void kamion2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                zabac.Top -= 20;
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                zabac.Top += 20;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                zabac.Left -= 20;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                zabac.Left += 20;
+            }
+        }
+
+        private void GUBITAK()
+        {
+            timer1.Stop();
+            MessageBox.Show("ИЗГУБИЛИ СТЕ!");
+            
+        }
+
+        private void POBEDA()
+        {
+            timer1.Stop();
+            MessageBox.Show("ПОБЕДИЛИ СТЕ!");
+
+        }
+
+        private void Sudaranje()
+        {
+            if(zabac.Bounds.IntersectsWith(car1.Bounds) || zabac.Bounds.IntersectsWith(car2.Bounds) || zabac.Bounds.IntersectsWith(car3.Bounds) || zabac.Bounds.IntersectsWith(car4.Bounds) ||
+                zabac.Bounds.IntersectsWith(car5.Bounds) || zabac.Bounds.IntersectsWith(car6.Bounds) || zabac.Bounds.IntersectsWith(car7.Bounds) || zabac.Bounds.IntersectsWith(car8.Bounds) ||
+                    zabac.Bounds.IntersectsWith(kamion1.Bounds) || zabac.Bounds.IntersectsWith(kamion2.Bounds) || zabac.Bounds.IntersectsWith(kamion3.Bounds))
+            {
+                GUBITAK();
+            }
+
+            if(zabac.Bounds.IntersectsWith(voda1.Bounds))
+            {
+                if(zabac.Bounds.IntersectsWith(panel1.Bounds) || zabac.Bounds.IntersectsWith(panel3.Bounds) || zabac.Bounds.IntersectsWith(panel4.Bounds) || zabac.Bounds.IntersectsWith(panel6.Bounds) ||
+                    zabac.Bounds.IntersectsWith(panel7.Bounds) || zabac.Bounds.IntersectsWith(panel9.Bounds))
+                {
+                    zabac.Left -= 2;
+                }
+                else if(zabac.Bounds.IntersectsWith(panel2.Bounds) || zabac.Bounds.IntersectsWith(panel5.Bounds) || zabac.Bounds.IntersectsWith(panel8.Bounds))
+                {
+                    zabac.Left += 5;
+                }
+                else
+                {
+                    GUBITAK();
+                }
+            }
+
+            if(zabac.Bottom <= jkra.Bottom)
+            {
+                POBEDA();
+            }
+        }
+    }
+}
